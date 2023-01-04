0ce835385a91a151f6c12906bbac86b27d8d4090
SecretGmG <cedric@sigrist.ch>
2023-01-04 17:36:46 +0100

    gha

--- src/main.rs
+++ src/main.rs
@@ -2,88 +2,92 @@
 
 mod nim;
 mod vec_ops;
-use std::{time::Instant, fs::File, io::Write};
-use nim::{pit::Pit, generalized::constructor};
-use nim::evaluator::Evaluator;
-
+use crate::nim::evaluator::impartial::{Impartial, ImpartialPart};
 use crate::nim::evaluator_db::DataBase;
 use crate::nim::generalized::GeneralizedNimGame;
-use crate::nim::pit_reconstructor::try_reconstruct;
+use nim::evaluator::Evaluator;
+use nim::generalized::closed_generalized::ClosedGeneralizedNimGame;
+use nim::{generalized::constructor::*, pit::Pit};
+use std::{fs::File, io::Write, time::Instant};
 
 fn main() {
-    time_both_eval(&constructor::rect(2, 8));
+    let mut generic_eval: Evaluator<ClosedGeneralizedNimGame, GeneralizedNimGame> =
+        Evaluator::new();
+    let mut old_eval = DataBase::new();
+    for i in 0..6 {
+        let test = Pit::empty_rect(i, i).get_generalized();
+
+        time_generic(&test, &mut generic_eval);
+        time_old_eval(&test, &mut old_eval);
+    }
 }
 
-fn time_both_eval(g: &GeneralizedNimGame){
-    println!("{}",g);
-    println!("old:");
-    time_old_eval(g);
-    println!("new:");
-    time_new_eval(g);
-}
-
-fn time_new_eval(g: &GeneralizedNimGame){
+fn time_generic<Part, Whole>(g: &Whole, eval: &mut Evaluator<Part, Whole>)
+where
+    Part: ImpartialPart<Part, Whole>,
+    Whole: Impartial<Part, Whole>,
+{
+    println!("Generic Evaluator, no bounds");
     let starting_test = Instant::now();
-    let mut eval = Evaluator::new();
     println!("{}", eval.calc_nimber(&g));
     println!("{:?}", starting_test.elapsed());
 }
-fn time_old_eval(g: &GeneralizedNimGame){
-    let starting_test = Instant::now();
-    let mut db = DataBase::new();
-    println!("{}", db.get_nimber(g));
 
+fn time_old_eval(g: &GeneralizedNimGame, db: &mut DataBase) {
+    println!("Old Evaluator");
+    let starting_test = Instant::now();
+    println!("{}", db.get_nimber(g));
     println!("{:?}", starting_test.elapsed());
 }
 #[allow(dead_code)]
-fn print_triangle_nimbers(max :u16) {
+fn print_triangle_nimbers(max: u16) {
     let mut db = DataBase::new();
-    for i in 0..max{
-        let g = nim::generalized::constructor::triangle(i);
-        println!("{}:{}", i,db.get_nimber(&g));
+    for i in 0..max {
+        let g = Constructor::triangle(i).build();
+        println!("{}:{}", i, db.get_nimber(&g));
     }
 }
 #[allow(dead_code)]
-fn make_and_write_data_base(p:Pit){
-
+fn make_and_write_data_base(p: Pit) {
     let mut db = DataBase::new();
 
     db.get_nimber(&p.get_generalized());
 
     let latex_repr = &*db.get_latex_repr();
     let mut f = File::create("db.tex").expect("Unable to create file");
-    f.write_all(latex_repr.as_bytes()).expect("Unable to write data");
-
+    f.write_all(latex_repr.as_bytes())
+        .expect("Unable to write data");
 }
 #[allow(dead_code)]
-fn test_reconstruction(pit: Pit){
+fn test_reconstruction(pit: Pit) {
     println!("{}", pit);
     let g = pit.get_generalized();
-    if g.get_parts().len() == 0 {return;}
+    if g.get_parts().len() == 0 {
+        return;
+    }
     let c = &g.get_parts()[0];
     println!("{}", c);
-    let reconstructed = try_reconstruct(c).unwrap();
+    let reconstructed = Pit::try_reconstruct(c).unwrap();
     println!("{}", reconstructed);
 }
 #[allow(dead_code)]
-fn get_latex_data_base(pit: Pit) -> String{
+fn get_latex_data_base(pit: Pit) -> String {
     let mut db = DataBase::new();
     db.get_nimber(&pit.get_generalized());
-    
+
     println!("{}", db);
 
     return db.get_latex_repr();
-
 }
 #[allow(dead_code)]
-fn test_pit_game(pit :Pit){
+fn test_pit_game(pit: Pit) {
     let starting_test = Instant::now();
 
     let db = &mut DataBase::new();
     println!("{}", pit);
 
     let g = pit.get_generalized();
-    
+
     println!("{}", g);
 
     let nimber = db.get_nimber(&g);
